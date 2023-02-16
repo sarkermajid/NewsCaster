9ab2e54b2497e44d3757925c88939e7e1567e30a
Md. Majidur Rahman Sarker <63114252+sarkermajid@users.noreply.github.com>
2023-02-16 17:18:38 +0600

    change

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -2,62 +2,56 @@
 
 
 @section('body')
-    <div class="main-content">
-        <div class="page-content">
-            <div class="container-fluid">
-                <!-- start page title -->
-                <div class="row py-3">
-                    <div class="col-md-12">
-                        <h4>Dashboard</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xl-6">
-                        <div class="white_card card_height_100 mb_30 social_media_card">
-                            <div class="media_card_body">
-                                <div class="media_card_list">
-                                    <div class="single_media_card">
-                                        <h4>Total Sale</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Order</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Vendor</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Customer</h4>
-                                        <h3></h3>
-                                    </div>
-                                </div>
-                            </div>
+    <!-- start page title -->
+    <div class="row py-3">
+        <div class="col-md-12">
+            <h4>Dashboard</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="white_card card_height_100 mb_30 social_media_card">
+                <div class="media_card_body">
+                    <div class="media_card_list">
+                        <div class="single_media_card">
+                            <h4>Total Sale</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Order</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Vendor</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Customer</h4>
+                            <h3></h3>
                         </div>
                     </div>
-                    <div class="col-xl-6">
-                        <div class="white_card card_height_100 mb_30 social_media_card">
-                            <div class="media_card_body">
-                                <div class="media_card_list">
-                                    <div class="single_media_card">
-                                        <h4>Total Sale</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Order</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Vendor</h4>
-                                        <h3></h3>
-                                    </div>
-                                    <div class="single_media_card">
-                                        <h4>Total Customer</h4>
-                                        <h3></h3>
-                                    </div>
-                                </div>
-                            </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="white_card card_height_100 mb_30 social_media_card">
+                <div class="media_card_body">
+                    <div class="media_card_list">
+                        <div class="single_media_card">
+                            <h4>Total Sale</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Order</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Vendor</h4>
+                            <h3></h3>
+                        </div>
+                        <div class="single_media_card">
+                            <h4>Total Customer</h4>
+                            <h3></h3>
                         </div>
                     </div>
                 </div>
